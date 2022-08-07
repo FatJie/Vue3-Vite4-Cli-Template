4a14290459b067e79c5416482d7a7126a8a0ef69
fatjie <372761781@qq.com>
2022-08-07 15:25:29 +0800

    refactor: 简易模板 init

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <!-- 路由的渲染出口 -->
+  <router-view />
+</template>
+
+<script lang="ts" setup>
+</script>
